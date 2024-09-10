b8ef4f97968a1a14fbc54266ec0f9f138007b957
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-09-09 22:29:10 -0300

    refactor: Update root route to point to sessions#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
-  get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
-
-  root 'home#index'
+  root 'sessions#new'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
