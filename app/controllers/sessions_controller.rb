8deffdbf4d394c1c9ea8b4116ea37547805e594e
Douglas Kurotaki <douglas.kurotaki.97@gmail.com>
2024-09-09 22:20:37 -0300

    refactor: Improve session creation and destruction logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,25 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.password == params[:password]
-      session[:user_id] = user.email
-      redirect_to root_path, notice: 'Logged in successfully'
+    user = User.find_by_email(session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      session[:user_email] = user.email
+      redirect_to profile_path, notice: I18n.t('sessions.create.success')
     else
-      flash[:alert] = 'Login ou senha inválidos'
+      flash[:alert] = I18n.t('sessions.create.failure')
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    redirect_to root_path
+    reset_session
+
+    redirect_to login_path, notice: I18n.t('sessions.destroy.success')
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
